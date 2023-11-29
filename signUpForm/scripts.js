9702ec180209b01b6ae6ef7bcc3a2d3e6a3b7d7f
Zytheta <112493405+Zytheta@users.noreply.github.com>
2023-11-29 13:11:56 +0900

    Started sign-up project

---
+++ signUpForm/scripts.js
@@ -0,0 +1,23 @@
+// Global Variables
+
+
+
+// Functions
+
+// DOM Events
+
+// Data Management
+
+// fetch('https://httpbin.org/get')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('GET request successful:', data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the GET request:', error);
+// });
